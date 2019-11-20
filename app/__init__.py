fed3e67ec0143ccc58763595af74249beb56ab05
ncallan <55205605+ncallan@users.noreply.github.com>
2019-11-19 20:36:28 -0500

    Add flask-moment for displaying/formatting times in client timezone

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
@@ -13,6 +14,8 @@ db = SQLAlchemy(app)
 login = LoginManager()
 login.init_app(app)
 login.login_view = "signin"
+
+moment = Moment(app)
 
 from app.db_model import *
 from app.routes import *
