46749505c7ee8ddda4d971826f16d46fdbff36c1
ncallan <55205605+ncallan@users.noreply.github.com>
2019-11-25 19:24:08 -0500

    Increase legibility of create_schema.py description

--- create_schema.py
+++ create_schema.py
@@ -1,8 +1,8 @@
 """This is a helper program to create a database schema.
 
-It will add all necessary tables to the database,
-as defined in the db.metadata object (in this case defined in db_model.py
-and imported in __init__.py). All pre-existing tables will be ignored.
+It will add all necessary tables to the database as defined in the db.metadata
+object (in this case defined in db_model.py and imported in __init__.py).
+All pre-existing tables will be ignored.
 """
 
 from app import db
